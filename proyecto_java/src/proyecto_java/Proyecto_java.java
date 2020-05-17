a15143bc6a7b4fe242c294b9df67586b7b6a572b
BrayanNolasco <brayannolasco011@gmail.com>
2020-05-16 18:15:44 -0600

    he agregado los codigos para hacer la multiplicacion

--- proyecto_java/src/proyecto_java/Proyecto_java.java
+++ proyecto_java/src/proyecto_java/Proyecto_java.java
@@ -16,6 +16,22 @@ public class Proyecto_java {
    public static void main(String[] args) {
     
    Scanner leer = new Scanner(System.in);
+   double numero1, numero2, M;
+    
+        System.out.println("----MULTIPLICACION-----");
+        
+        System.out.println("Digite su primer valor");
+        numero1 = leer.nextDouble();
+        
+         System.out.println("Digite su segundo valor");
+        numero2 = leer.nextDouble();
+        
+        M = numero1 * numero2;
+        
+        System.out.println("-----------------------");
+        System.out.println("Resultado" + M);
+        System.out.println("-----------------------");
+        
    
    }
     
